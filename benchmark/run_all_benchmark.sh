5db6e5bdf39c1bf43a6f1ff3fc1e73082c7f1d7e
David Jiménez <david0861@gmail.com>
2024-10-01 14:45:24 +0200

    Update run_all_benchmark.sh

--- benchmark/run_all_benchmark.sh
+++ benchmark/run_all_benchmark.sh
@@ -1,8 +1,8 @@
-
+#!/usr/bin/env bash
 export MODEL_PATH="large-v3"
 export LANG="es"
 export COMPUTE_TYPE="float16"
 
 python3 speed_benchmark.py
 python3 memory_benchmark.py
-python3 wer_benchmark.py
+python3 wer_benchmark.py  # Uses: dataset = load_dataset("librispeech_asr", "clean", split="validation", streaming=True)
